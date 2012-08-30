e0e40f82bdc5305d043380c15ea8bc59ff43ffa3
unknown <amitshar@KUDUTEST.redmond.corp.microsoft.com>
2012-08-29 21:32:54 -0700

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication_35.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC! Second deployment";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
